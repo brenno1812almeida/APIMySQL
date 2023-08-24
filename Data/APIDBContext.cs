dc9e29b898cc79a494b97a0cc2c8ed68e06a59e0
Brenno Almeida <brenno1812almeida@gmail.com>
2023-08-24 00:25:07 -0300

    API .NET Core utilizando o MySQL

---
+++ Data/APIDBContext.cs
@@ -0,0 +1,14 @@
+﻿using APIMySQL.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIMySQL.Data
+{
+    public class APIDBContext : DbContext
+    {
+        public APIDBContext(DbContextOptions<APIDBContext> options) : base(options) { 
+        
+        }
+
+        public DbSet<Estado> Estado { get; set; }
+    }
+}
---
+++ Model/Estado.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APIMySQL.Model
+{
+    public class Estado
+    {
+        [Key]
+        [StringLength(2, MinimumLength = 2, ErrorMessage = "O campo sigla deve ter 2 caracteres")]
+        public string Sigla { get; set; }
+
+        [Required(ErrorMessage = "O campo nome é obrigatório")]
+        [StringLength(200, MinimumLength = 3, ErrorMessage = "O campo sigla deve ter entre 3 a 200 caracteres")]
+        public string Nome { get; set; }
+    }
+}
